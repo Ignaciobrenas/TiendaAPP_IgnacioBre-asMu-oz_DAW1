9f696331ddc1718b22df55588a053e078f75fce5
Ignacio <ignaciobrenas@gmail.com>
2026-01-22 11:53:36 +0100

    Tienda 1.5 (Clases creadas)

---
+++ src/model/Client.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import model.Amount;
+
+/**
+ *
+ * @author ignac
+ */
+public class Client extends Person{
+    int memberId;
+    Amount balance; 
+
+    public Client(int memberId, Amount balance, String name) {
+        super(name);
+        this.memberId = memberId;
+        this.balance = balance;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public Amount getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Amount balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "memberId=" + memberId + ", balance=" + balance + '}';
+    }
+    
+}
---
+++ src/model/Employee.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ignac
+ */
+public class Employee extends Person {
+    int employeeId;
+    String password;
+
+    public Employee(int employeeId, String password, String name) {
+        super(name);
+        this.employeeId = employeeId;
+        this.password = password;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "employeeId=" + employeeId + ", password=" + password + '}';
+    }
+    
+}
